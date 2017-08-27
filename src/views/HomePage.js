06eade409a06b2084d784f3ef3d658429dc48392
Ryan McKenna <ryan@rekenna.com>
2017-08-27 02:58:41 -0600

    Updated README.md and Home Page intro content

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -24,10 +24,8 @@ export default class HomePage extends Component{
             </div>
             <div className="col-md-6">
               <article>
-                <h3>Welcome to WoW Passport v1.0</h3>
-                <p>Ever wondered how some graphic designers always manage to produce beautiful looking designs for their brochures, website designs, logo designs? Talent…yes, it helps but there are a handful of more important things you can do that will have even complete beginners producing award winning design.</p>
-                <p>Forget everything you’ve ever been told about Graphic Design.</p>
-                <p>There are some hard and fast things that graphic designers will insist are to be obeyed. Only use limited fonts on a design for example or never use green on a magazine or book cover as people will not buy it. Stuff and nonsense.</p>
+                <h3>Welcome to WoW Passport v1.0.0</h3>
+                <p>I'm still working on this application every day, so expect bugs! You are welcome to follow my updates on the <a href="https://github.com/Rekenna/WoW-Passport" rel="noopener noreferrer" target="_blank">github repository</a> and if you have any suggestions feel free to leave a comment on the repository.</p>
               </article>
               {this.props.children}
             </div>
