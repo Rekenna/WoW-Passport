d2d7505e041deefd6d4de63c22e2c69f3a42f155
Ryan McKenna <ryan@rekenna.com>
2017-09-05 13:15:23 -0600

    Feedback form states

--- src/app/components/Feedback/FeedbackForm.js
+++ src/app/components/Feedback/FeedbackForm.js
@@ -11,8 +11,10 @@ export default class FeedbackForm extends Component {
     super()
 
     this.state = {
-      formOpen: false
+      progress: "closed"
     }
+
+    this._sendSlackMessage = this._sendSlackMessage.bind(this)
   }
 
   _handleSubmit(e){
@@ -22,14 +24,28 @@ export default class FeedbackForm extends Component {
   }
 
   _openForm(e){
-      this.setState({
-        formOpen : !this.state.formOpen
-      })
+      const progress = this.state.progress
+      if(progress === "done"){
+        return
+      }
+      else if(progress === "open"){
+        this.setState({
+          progress : 'closed'
+        })
+      }
+      else{
+        this.setState({
+          progress : 'open'
+        })
+      }
+      return
   }
 
   _sendSlackMessage(name, email, message){
+
     let url = `https://hooks.slack.com/services/${slack}`
     let text = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+
     $.ajax({
         data: 'payload=' + JSON.stringify({
             "text": text
@@ -39,39 +55,62 @@ export default class FeedbackForm extends Component {
         type: 'POST',
         url: url
     });
+    this.setState({
+      progress: "done"
+    })
     return
   }
   render(){
 
+    let icon;
     let form;
-    if(this.state.formOpen){
-      form = (
-        <form className="feedback-form" onSubmit={this._handleSubmit.bind(this)}>
-          <div className="form-group">
-            <label>Name</label>
-            <input ref={c => this._name = c} type="text" className="form-control" placeholder="Johnny Awesome"/>
+    const progress = this.state.progress
+
+    switch (progress) {
+      case "done" :
+        icon = (<i className="fa fa-thumbs-up"></i>);
+        break;
+      case "open" :
+        icon = (<i className="fa fa-close"></i>);
+        form = (
+          <div className="feedback-form-content">
+            <div className="panel-heading">
+              <h3 className="panel-title">Provide Feedback</h3>
+            </div>
+            <div className="panel-body">
+              <form onSubmit={this._handleSubmit.bind(this)}>
+                <div className="form-group">
+                  <label>Name</label>
+                  <input ref={c => this._name = c} type="text" className="form-control" placeholder="Johnny Awesome"/>
+                </div>
+                <div className="form-group">
+                  <label>Email</label>
+                  <input ref={c => this._email = c} type="email" className="form-control" placeholder="johnny@awesome.com"/>
+                </div>
+                <div className="form-group">
+                  <label>Message</label>
+                  <textarea ref={c => this._message = c} className="form-control" placeholder="Here's my awesome message. What were you expecting?"></textarea>
+                </div>
+                <div className="form-group">
+                  <button type="submit" className="submit-btn">Send Message <i className="fa fa-send"></i></button>
+                </div>
+              </form>
+            </div>
           </div>
-          <div className="form-group">
-            <label>Email</label>
-            <input ref={c => this._email = c} type="email" className="form-control" placeholder="johnny@awesome.com"/>
-          </div>
-          <div className="form-group">
-            <label>Message</label>
-            <textarea ref={c => this._message = c} className="form-control" placeholder="Here's my awesome message. What were you expecting?"></textarea>
-          </div>
-          <div className="form-group">
-            <button type="submit" className="submit-btn">Send Message</button>
-          </div>
-        </form>
-      );
+        );
+        break;
+      default:
+        icon = (<i className="fa fa-comment"></i>);
     }
 
     return(
       <div className="feedback-form-container">
         <div className="feedback-button" onClick={this._openForm.bind(this)}>
-          <i className="fa fa-comment"></i>
+          {icon}
         </div>
-        {form}
+        <div className={`feedback-form panel panel-default ${this.state.progress}`}>
+          {form}
+        </div>
       </div>
     );
   }
